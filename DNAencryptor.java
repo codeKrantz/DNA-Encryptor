acb7cab6183d2f46686a3bbbee730be649e9d0cf
Conrad Krantz <conradkrantz@gmail.com>
2024-03-17 22:21:10 -0500

    initialized basic class and changed file names

---
+++ DNAencryptor.java
@@ -0,0 +1,12 @@
+public class DNAencryptor{
+    private String message;
+
+
+    public DNAencryptor(String messageInput){
+        message = messageInput;
+    }
+
+    public String getMessage(){
+        return message;
+    }
+}
--- main.java
+++ main.java
@@ -2,6 +2,16 @@ class Main{
 
     public static void main(String[] args){
 
+        //basic message info
+        String test = "a b c d %";
+        String uppercaseMessage = test.toUpperCase(); //all upercase reduces the number of charaters needed for base pairing
+        
+        //initializing class
+        DNAencryptor DNA = new DNAencryptor(uppercaseMessage);
+        System.out.println(DNA.getMessage());
+
+
+
     }
 
 }
