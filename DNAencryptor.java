aba747f31b179fe634b74b85c6f3abd7d12b7de5
Conrad Krantz <conradkrantz@gmail.com>
2024-03-25 20:07:04 -0500

    primative access to letters added

--- DNAencryptor.java
+++ DNAencryptor.java
@@ -2,11 +2,23 @@ public class DNAencryptor{
     private String message;
 
 
+
     public DNAencryptor(String messageInput){
         message = messageInput;
+        
     }
 
-    public String getMessage(){
-        return message;
+    public void getMessage(){
+        // create a char list from the string message
+       char[] charList = new char[message.length()-1];
+
+       //use get char method to access letters are primative data types
+       try {
+        message.getChars(0, 2, charList, 0);
+        System.out.println(charList);
+       }   
+       catch (Exception ex) {
+        System.out.println(ex);
+    } 
     }
 }
--- main.java
+++ main.java
@@ -3,12 +3,12 @@ class Main{
     public static void main(String[] args){
 
         //basic message info
-        String test = "a b c d %";
+        String test = "test";
         String uppercaseMessage = test.toUpperCase(); //all upercase reduces the number of charaters needed for base pairing
         
         //initializing class
         DNAencryptor DNA = new DNAencryptor(uppercaseMessage);
-        System.out.println(DNA.getMessage());
+        DNA.getMessage();
 
 
 
