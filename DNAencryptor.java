591b24cb26374343448942dc9a8618bf8f3f4e63
Conrad Krantz <conradkrantz@gmail.com>
2024-03-26 19:14:56 -0500

    added a precodition

--- DNAencryptor.java
+++ DNAencryptor.java
@@ -8,6 +8,15 @@ public class DNAencryptor{
         
     }
 
+/*
+ * PRECODITION: message does not contrain lower case letters
+ * Punctuation will then come next in the key
+ * then number starting with 1 to 9 and ending at zero (follow the key board)
+ * special characters may be add based on logical use
+ * start and end codons will be added based on the key
+ * follow the key from the top left and work your way down and then back up one collum to the right 
+ */
+
     public void getMessage(){
         // create a char list from the string message and store last char index
        char[] charList = new char[message.length()];
@@ -15,6 +24,11 @@ public class DNAencryptor{
 
        //use get char method to access letters are primative data types
         message.getChars(0, lastChar, charList, 0);
+
+
+        //printing out the message Starting with the start codon
+        System.out.println("3-MET");
+        // then go down the key starting with letters and after Z switch to . , ! and ? then move to numbers 1-0
         
         
        
