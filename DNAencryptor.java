f93e7d88da48f361e34edc1bb70b12b8deb019c5
Conrad Krantz <conradkrantz@gmail.com>
2024-03-26 19:01:20 -0500

    fixed indexing issues

--- DNAencryptor.java
+++ DNAencryptor.java
@@ -9,16 +9,15 @@ public class DNAencryptor{
     }
 
     public void getMessage(){
-        // create a char list from the string message
-       char[] charList = new char[message.length()-1];
+        // create a char list from the string message and store last char index
+       char[] charList = new char[message.length()];
+       int lastChar = message.length();
 
        //use get char method to access letters are primative data types
-       try {
-        message.getChars(0, 2, charList, 0);
-        System.out.println(charList);
-       }   
-       catch (Exception ex) {
-        System.out.println(ex);
-    } 
-    }
+        message.getChars(0, lastChar, charList, 0);
+        
+        
+       
+    
+}
 }
